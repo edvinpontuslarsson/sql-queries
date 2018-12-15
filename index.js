899f44609fe9660c4400436bad6b306db8f93fd1
Edvin Larsson <edvinpontuslarsson@gmail.com>
2018-12-15 14:22:05 +0100

    gets comment from latest datetime

--- index.js
+++ index.js
@@ -23,8 +23,10 @@ function performQueries(db) {
     // for specific user, find someone with multiple posts
     // 2007 file - user: gigaquack, subreddit_id: t5_2fwo (programming)
 
-    // amount of comments by specific user:
     const specificUser = 'gigaquack'
+    const specificSubredditID = 't5_2fwo'
+
+    // amount of comments by specific user:
     const sqlUserCommentsAmount = 
         `SELECT * FROM Comments WHERE author = '${specificUser}'`
     db.query(sqlUserCommentsAmount, (err, result) => {
@@ -34,5 +36,13 @@ function performQueries(db) {
         )
     })
 
+    // amount of comments a specific subreddit gets per day
 
+    // date of first and date of last, get amount of days
+
+    const sqlGetFirstRow = 'SELECT * FROM Comments ORDER BY created_utc DESC LIMIT 1'
+    db.query(sqlGetFirstRow, (err, result) => {
+        if (err) throw err
+        console.log(result)
+    })
 }
